f0ec00b59d02f0b90191b4f4edfbd2a791243021
Sofus633 <105514939+Sofus633@users.noreply.github.com>
2024-10-08 10:14:45 +0200

    Update 2D fighter

---
+++ 2D fighter/hitboxcreator.py
@@ -0,0 +1,26 @@
+import pygame 
+
+running = True
+hitboxes = []
+
+
+
+def main():
+    while running:
+        display()
+        checkinputs()
+
+
+def checkinputs():
+    keys = pygam.getkeys()
+    if 
+
+def display():
+    for hitbox in hitboxes:
+        pygam.draw.rect()  
+
+
+
+
+if __name__ == "__main__":
+    main()

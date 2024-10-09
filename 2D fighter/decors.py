1cbd0bc42386585916b7920bc166a5b9c57948ae
Sofus633 <105514939+Sofus633@users.noreply.github.com>
2024-10-09 18:28:08 +0200

    Delete 2D fighter directory

--- 2D fighter/decors.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-
-class Bloc:
-    def __init__(self, position, size) :
-        self.position = position
-        self.size = size
-        self.apparence = pygame.Rect(self.position, self.size) #hitbox
